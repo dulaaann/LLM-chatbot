b4cf3cd0d6fbbfa41aeb4350adffe0b36dd1b446
dulanherath <dulanmherath@outlook.com>
2025-07-14 10:10:58 +0530

    completed

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,8 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_google_genai import ChatGoogleGenerativeAI
 import json
 
 with open("env.json") as f:
@@ -40,6 +42,7 @@ if file is not None:
     google_api_key=GOOGLE_API_KEY
 )
 
+
 #Creating vector store
     vector_store = FAISS.from_texts(chunks,embeddings)
 
@@ -47,5 +50,16 @@ if file is not None:
     user_question = st.text_input("Type the question")
 #do similarity search
     if user_question : 
-        vector_store.similarity_search(user_question)
+        matches = vector_store.similarity_search(user_question,k=3)
+        # for match in matches:
+        #     st.write(match.page_content)
+        llm = ChatGoogleGenerativeAI(
+            model="gemini-2.0-flash-exp",
+            google_api_key=GOOGLE_API_KEY,
+            temperature=0,
+            max_tokens=1000
+            )
 #output
+        chain = load_qa_chain(llm,chain_type="stuff")
+        response = chain.run( input_documents = matches, question = user_question)
+        st.write(response)
